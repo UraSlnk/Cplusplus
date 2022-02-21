42abafa57dc1648b4a6e23ba613eea00cc85b0e1
UraSlnk <joricas@yandex.ru>
2022-02-21 22:49:53 +0300

    logic update
    TODO: implements externalizable
    make button save-load

--- javaFXdemo/src/main/java/com/example/javafxdemo/CalculateButtonClickedController.java
+++ javaFXdemo/src/main/java/com/example/javafxdemo/CalculateButtonClickedController.java
@@ -1,9 +1,15 @@
 package com.example.javafxdemo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class CalculateButtonClickedController {
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class CalculateButtonClickedController implements Externalizable {
 
     public TextField hotPrice;
     public TextField coldPrice;
@@ -20,12 +26,38 @@ public class CalculateButtonClickedController {
     public TextField coldNewValue;
     public TextField energyNewValue;
 
+    public TextField hotPay;
+    public TextField coldPay;
+    public TextField energyPay;
+    public TextField waterEjectPay;
+    public TextField monthlyPay;
+
+    public TextField calculatePayment;
+    public Label exceptionLabel;
+
     //Локальные переменные для хранения распарсенных значений из строк
     private double hotPriceD = 0.0;
     private double coldPriceD = 0.0;
     private double energyPriceD = 0.0;
     private double waterEjectPriceD = 0.0;
     private double monthlyPriceD = 0.0;
+
+    private double hotOldValueD = 0.0;
+    private double coldOldValueD = 0.0;
+    private double energyOldValueD = 0.0;
+    private double waterEjectValueD = 0.0;
+
+    private double hotNewValueD = 0.0;
+    private double coldNewValueD = 0.0;
+    private double energyNewValueD = 0.0;
+
+    private double hotPayD;
+    private double coldPayD;
+    private double energyPayD;
+    private double waterEjectPayD;
+    private double monthlyPayD;
+
+    private double calculatePaymentD = 0.0;
 
     @FXML
     protected void onCalculateButtonClick() {
@@ -35,14 +67,46 @@ public class CalculateButtonClickedController {
             energyPriceD = Double.parseDouble(energyPrice.getText().trim().replaceAll(",", "."));
             waterEjectPriceD = Double.parseDouble(waterEjectPrice.getText().trim().replaceAll(",", "."));
             monthlyPriceD = Double.parseDouble(monthlyPrice.getText().trim().replaceAll(",", "."));
+
+            hotOldValueD = Double.parseDouble(hotOldValue.getText().trim().replaceAll(",", "."));
+            coldOldValueD = Double.parseDouble(coldOldValue.getText().trim().replaceAll(",", "."));
+            energyOldValueD = Double.parseDouble(energyOldValue.getText().trim().replaceAll(",", "."));
+
+            hotNewValueD = Double.parseDouble(hotNewValue.getText().trim().replaceAll(",", "."));
+            coldNewValueD = Double.parseDouble(coldNewValue.getText().trim().replaceAll(",", "."));
+            energyNewValueD = Double.parseDouble(energyNewValue.getText().trim().replaceAll(",", "."));
+
+            waterEjectValueD = (coldNewValueD - coldOldValueD) + (hotNewValueD - hotOldValueD);
+            waterEjectPayD = waterEjectValueD * waterEjectPriceD;
+
+            waterEjectValue.setText(String.format("%.2f", waterEjectValueD));
+            coldPay.setText(String.format("%.2f", (coldNewValueD - coldOldValueD) * coldPriceD));
+            hotPay.setText(String.format("%.2f", (hotNewValueD - hotOldValueD) * hotPriceD));
+            energyPay.setText(String.format("%.2f", (energyNewValueD - energyOldValueD) * energyPriceD));
+            waterEjectPay.setText(String.format("%.2f", waterEjectPayD));
+            monthlyPay.setText(String.format("%.2f", monthlyPriceD));
+
+            energyPayD = (energyNewValueD - energyOldValueD) * energyPriceD;
+            coldPayD = (coldNewValueD - coldOldValueD) * coldPriceD;
+            hotPayD = (hotNewValueD - hotOldValueD) * hotPriceD;
+            calculatePaymentD = waterEjectPayD + monthlyPriceD + energyPayD + coldPayD + hotPayD;
+            calculatePayment.setText(String.format("%.2f", calculatePaymentD));
+
+            exceptionLabel.setText("");
+
         } catch (NumberFormatException nfe){
             System.out.println("Вводите числа разделенные '.' и ',' в текстовые поля");
+            exceptionLabel.setText("Проверьте, все ли поля заполнены правильно");
         }
+    }
 
-        System.out.println(hotPriceD);
-        System.out.println(coldPriceD);
-        System.out.println(energyPriceD);
-        System.out.println(waterEjectPriceD);
-        System.out.println(monthlyPriceD);
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
     }
 }
